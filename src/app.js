27e057bbd57dc5a3a00701a5c760e655adffd5be
andre.r.leao <andre.leao.xoom@gmail.com>
2020-08-05 18:36:11 -0300

    api de teste.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const port = process.env.PORT
-const userRouter = require('./routers/user')
-require('./db/db')
+const express = require('express');
+const port = process.env.PORT;
+const userRouter = require('./routers/user');
+require('./db/db');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(userRouter)
+app.use(express.json());
+app.use(userRouter);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+    console.log(`Server running on port ${port}`);
+});
